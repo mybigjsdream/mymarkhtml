32bd30ce79826ec320d303189f5ef5e845bc9c48
邓景 <dengjing@dengjingdeMacBook-Pro.local>
2015-12-30 16:19:53 +0800

    a litter change

--- app.js
+++ app.js
@@ -7,28 +7,17 @@ if (Meteor.isClient) {
         }
     });
 
-    //var textarea_handle = function(e){
-    //    console.log('ssss');
-    //e.preventDefault();
-    //Session.set('text', t.$('[name=textarea]').val());
-    //var text =  $(e.target).find('[class=textarea]').val();
-    //console.log(text);
-    //Session.set('text', text);
-    //};
-
     Template.input.events({
         'input textarea': function(e, t) {
             e.preventDefault();
             Session.set('text', t.$('[name=textarea]').val());
-            //console.log($(e.target).find('[name=textarea]'));
+            var $textarea = $(e.target).find('[name=textarea]');
             console.log(t.$('[name=textarea]'));
-            //console.log($(e.target).find('[name=textarea]').val());
             console.log($(e.target).find('[name=textarea]').context.scrollHeight);
-            t.$('[name=textarea]').height(100);
-            //$(e.target).find('[name=textarea]').height(100);
-            var tt = $(e.target).find('[name=hahah]').val();
-            //console.log($ttt.val('hahasds'));
-            //tt = 'dengjing';
+            console.log($(e.target).find('[name=textarea]').context.clientHeight);
+            if ($textarea.context.scrollHeight > $textarea.context.clientHeight) {
+                t.$('[name=textarea]').height($(e.target).find('[name=textarea]').context.scrollHeight);
+            }
         },
     });
 
