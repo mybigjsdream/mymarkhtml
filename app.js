645d5bbde180c003fa3849aa724963579175c84d
邓景 <dengjing@dengjingdeMacBook-Pro.local>
2015-12-30 16:55:12 +0800

    a litter change

--- app.js
+++ app.js
@@ -8,15 +8,14 @@ if (Meteor.isClient) {
     });
 
     Template.input.events({
-        'input textarea': function(e, t) {
-            e.preventDefault();
+        'keydown textarea': function(e, t) {
             Session.set('text', t.$('[name=textarea]').val());
             var $textarea = $(e.target).find('[name=textarea]');
-            console.log(t.$('[name=textarea]'));
-            console.log($(e.target).find('[name=textarea]').context.scrollHeight);
-            console.log($(e.target).find('[name=textarea]').context.clientHeight);
             if ($textarea.context.scrollHeight > $textarea.context.clientHeight) {
-                t.$('[name=textarea]').height($(e.target).find('[name=textarea]').context.scrollHeight);
+                t.$('[name=textarea]').height($textarea.context.scrollHeight);
+            }
+            if (e.keyCode == 8) {
+                console.log('!!');
             }
         },
     });
